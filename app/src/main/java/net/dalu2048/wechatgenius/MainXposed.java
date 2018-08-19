75dc4b65d153863bd3f6066848cec3cea180cec5
lilu <1801099979@qq.com>
2018-08-19 19:53:42 +0800

    实时获取到微信聊天消息，hook数据库插入操作。

--- app/src/main/java/net/dalu2048/wechatgenius/MainXposed.java
+++ app/src/main/java/net/dalu2048/wechatgenius/MainXposed.java
@@ -10,16 +10,69 @@
 
 package net.dalu2048.wechatgenius;
 
+import android.content.ContentValues;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public final class MainXposed implements IXposedHookLoadPackage {
+    //微信数据库包名称
+    private static final String WECHAT_DATABASE_PACKAGE_NAME = "com.tencent.wcdb.database.SQLiteDatabase";
+    //微信主进程名
+    private static final String WECHAT_PROCESS_NAME = "com.tencent.mm";
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.processName.equals("com.tencent.mm")){
+        if (!lpparam.processName.equals(WECHAT_PROCESS_NAME)) {
             return;
         }
         XposedBridge.log("进入微信进程：" + lpparam.processName);
+        //调用 hook数据库插入。
+        hookDatabaseInsert(lpparam);
     }
+
+    //hook数据库插入操作
+    private void hookDatabaseInsert(final XC_LoadPackage.LoadPackageParam loadPackageParam) {
+        Class<?> classDb = XposedHelpers.findClassIfExists(WECHAT_DATABASE_PACKAGE_NAME, loadPackageParam.classLoader);
+        if (classDb == null) {
+            XposedBridge.log("hook数据库insert操作：未找到类" + WECHAT_DATABASE_PACKAGE_NAME);
+            return;
+        }
+        XposedHelpers.findAndHookMethod(classDb,
+                "insertWithOnConflict",
+                String.class, String.class, ContentValues.class, int.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        String tableName = (String) param.args[0];
+                        ContentValues contentValues = (ContentValues) param.args[2];
+                        if (tableName == null || tableName.length() == 0 || contentValues == null) {
+                            return;
+                        }
+                        //过滤掉非聊天消息
+                        if (!tableName.equals("message")) {
+                            return;
+                        }
+                        //打印出日志
+                        printInsertLog(tableName, (String) param.args[1], contentValues, (Integer) param.args[3]);
+                    }
+                });
+    }
+
+    //输出插入操作日志
+    private void printInsertLog(String tableName, String nullColumnHack, ContentValues contentValues, int conflictValue) {
+        String[] arrayConflicValues =
+                {"", " OR ROLLBACK ", " OR ABORT ", " OR FAIL ", " OR IGNORE ", " OR REPLACE "};
+        if (conflictValue < 0 || conflictValue > 5) {
+            return;
+        }
+        XposedBridge.log("Hook数据库insert。table：" + tableName
+                + "；nullColumnHack：" + nullColumnHack
+                + "；CONFLICT_VALUES：" + arrayConflicValues[conflictValue]
+                + "；contentValues:" + contentValues);
+    }
+
 }
